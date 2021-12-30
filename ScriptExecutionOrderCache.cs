9467541125aae12b41e19bf4ecf57e8ee2673bd1
Kiera Lord <>
2021-12-30 18:03:02 +1000

    Disabled cache as it relies on Cratesmith.Util which is depricated

--- ScriptExecutionOrderCache.cs
+++ ScriptExecutionOrderCache.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿/*
+using System.Collections.Generic;
 using System.Linq;
 using Cratesmith.Utils;
 using UnityEditor;
@@ -145,3 +146,4 @@ namespace Cratesmith.ScriptExecutionOrder
         }
     }
 }
+*/
